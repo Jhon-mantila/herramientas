e7780a83b62389aab9e1c7e45d6008d83a3961ff
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-09-03 12:21:45 -0500

    Agregando Imanges a los fragmentos, y agregando fragmento del menu a la actividad herramienta

--- app/src/main/java/com/example/herramientas/MainActivity.java
+++ app/src/main/java/com/example/herramientas/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.herramientas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ComunicaMenu{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void menu(int queBoton) {
+
+        Intent in = new Intent(this, ActividadHerramientas.class);
+
+        in.putExtra("BOTON_PULSADO", queBoton);
+
+        startActivity(in);
     }
 }
