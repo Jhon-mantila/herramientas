bca12eff27a989416a4c6a1643df009717edb930
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-09-03 14:32:06 -0500

    Configuración de los fragmentos de cada boton

--- app/src/main/java/com/example/herramientas/ActividadHerramientas.java
+++ app/src/main/java/com/example/herramientas/ActividadHerramientas.java
@@ -1,14 +1,42 @@
 package com.example.herramientas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import android.os.Bundle;
 
-public class ActividadHerramientas extends AppCompatActivity {
+public class ActividadHerramientas extends AppCompatActivity implements ComunicaMenu{
+
+    Fragment[] misFragmentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_herramientas);
+
+        misFragmentos = new Fragment[3];
+
+        misFragmentos[0] = new Linterna();
+        misFragmentos[1] = new Musica();
+        misFragmentos[2] = new Nivel();
+
+        Bundle extras = getIntent().getExtras();
+
+        menu(extras.getInt("BOTON_PULSADO"));
+    }
+
+    public void menu(int queBoton){
+
+        FragmentManager miManejador = getSupportFragmentManager();
+
+        FragmentTransaction miTransicion = miManejador.beginTransaction();
+
+        miTransicion.replace(R.id.herramientas, misFragmentos[queBoton]);
+
+        miTransicion.commit();
+
     }
 }
